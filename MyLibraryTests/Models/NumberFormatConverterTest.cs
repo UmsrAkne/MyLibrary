5eecf651dc2c22e9d223d4e87f76bd16d3f507b3
Akn <sibata486@gmail.com>
2023-12-26 21:56:40 +0900

    add test / NumberFormatConverter のテストを追加

---
+++ MyLibraryTests/Models/NumberFormatConverterTest.cs
@@ -0,0 +1,35 @@
+using MyLibrary.Converters;
+using NUnit.Framework;
+
+namespace MyLibraryTests.Models
+{
+    public class NumberFormatConverterTest
+    {
+        [Test]
+        [TestCase(1, "D4", "0001")]
+        [TestCase(10, "D4", "0010")]
+        [TestCase(1, "D3", "001")]
+        [TestCase(10, "D3", "010")]
+        [TestCase(1, "D1", "1")]
+        [TestCase(10, "D1", "10")]
+        public void ConvertTest(int input, string format, string result)
+        {
+            var c = new NumberFormatConverter()
+            {
+                Format = format,
+            };
+
+            Assert.That(c.Convert(input, typeof(string), null, null), Is.EqualTo(result));
+        }
+
+        public void ConvertTest_0の時の非表示のテスト()
+        {
+            var c = new NumberFormatConverter
+            {
+                VisibleWhenZero = true,
+            };
+
+            Assert.That(c.Convert(0, typeof(string), null, null), Is.EqualTo(string.Empty));
+        }
+    }
+}
