60222eeb1b41baaa92f781dfe7993dcb0e78a36d
UmsrAkne <sibata486@gmail.com>
2025-05-05 15:25:35 +0900

    refactor: simplify AppVersionInfo to use assembly version metadata

--- MyLibrary/Utils/AppVersionInfo.cs
+++ MyLibrary/Utils/AppVersionInfo.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using Prism.Mvvm;
@@ -7,60 +6,23 @@ namespace MyLibrary.Utils
 {
     public class AppVersionInfo : BindableBase
     {
-        private string title;
-        private string version = string.Empty;
-
         public AppVersionInfo()
         {
             var projectName = Assembly.GetExecutingAssembly().GetName().Name ?? string.Empty;
             ProjectName = Regex.Replace(projectName, "([a-z])([A-Z])", "$1 $2");
-            Title = ProjectName;
-
-            SetVersion();
-            AddDebugMark();
         }
 
-        public string Title
-        {
-            get => string.IsNullOrWhiteSpace(Version)
-                ? title
-                : title + "   version : " + Version;
-            private set => SetProperty(ref title, value);
-        }
-
-        public int MajorVersion { get; init; }
-
-        public int MinorVersion { get; init; }
-
-        public int PatchVersion { get; init; }
-
-        // ReSharper disable once CommentTypo
-        /// <summary>
-        /// 最終アップデートの日付を `YYYYmmdd` のフォーマットで入力します。
-        /// </summary>
-        public string Updated { get; init; } = "";
-
-        public string SuffixId { get; init; } = "a";
+        public string Title => GetAppNameWithVersion();
 
         private string ProjectName { get; }
 
-        private string Version { get => version; set => SetProperty(ref version, value); }
-
-        public void UpdateTitle()
+        public string GetAppNameWithVersion()
         {
-            Version = $"{MajorVersion}.{MinorVersion}.{PatchVersion} ({Updated}{SuffixId})";
-        }
-
-        [Conditional("RELEASE")]
-        private void SetVersion()
-        {
-            UpdateTitle();
-        }
-
-        [Conditional("DEBUG")]
-        private void AddDebugMark()
-        {
-            Title += " (Debug)";
+            var assembly = Assembly.GetExecutingAssembly();
+            var infoVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            return !string.IsNullOrWhiteSpace(infoVersion)
+                ? $"{ProjectName} ver:{infoVersion}"
+                : $"{ProjectName} (version unknown)";
         }
     }
 }
